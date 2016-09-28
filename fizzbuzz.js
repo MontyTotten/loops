8f02e98de3b1e2bd8c0ced4ae57643cc7ca23263
Montague Totten <monty.n.totten@gmail.com>
2016-09-28 16:06:01 -0400

    Corrected order and streamlined js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,14 @@
-
-var str = ' ';
-
 for (i = 0; i < 100; i++) { 
-	if (i % 3 === 0) {
-		console.log('Fizz') 
-			if (i % 5 === 0) {
+	if (i % 3 === 0 && i % 5 === 0) {
+		console.log('FizzBuzz')
+		}	 else if (i % 3 === 0) {
+				console.log('Fizz') }
+				else {
 				console.log('Buzz')
-					if (i % 3 === 0 && i % 5 === 0) {
-						console.log('FizzBuzz')
-					}
+									
 			}
 		}
-	}	
+
 
 
 
